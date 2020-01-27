062311a0ccae1954b95512c2725458062f4f4e7f
Ken Sodemann <ken@ionic.io>
2020-01-27 05:32:21 -0600

    feat(data): abstract database into own service

---
+++ src/app/services/database/database.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+import { DbTransaction, SQLite, SQLiteObject } from '@ionic-enterprise/offline-storage/ngx';
+
+interface Column {
+  name: string;
+  type: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  public handle: SQLiteObject;
+  private readyPromise: Promise<boolean>;
+  private isReady = false;
+
+  constructor(private platform: Platform, private sqlite: SQLite) {
+    this.readyPromise = this.initializeSchema();
+  }
+
+  async ready(): Promise<boolean> {
+    if (!this.isReady) {
+      await this.readyPromise;
+    }
+    return true;
+  }
+
+  private async initializeSchema(): Promise<boolean> {
+    await this.platform.ready();
+    await this.open();
+    return this.handle
+      .transaction(tx => {
+        this.createTables(tx);
+      })
+      .then(() => {
+        this.isReady = true;
+        return true;
+      });
+  }
+
+  private async open(): Promise<void> {
+    this.handle = await this.sqlite.create({
+      name: 'teaisforme.db',
+      location: 'default'
+    });
+  }
+
+  private createTables(transaction: DbTransaction): void {
+    const id = { name: 'id', type: 'INTEGER PRIMARY KEY' };
+    const name = { name: 'name', type: 'TEXT' };
+    const description = { name: 'description', type: 'TEXT' };
+    transaction.executeSql(
+      this.createTableSQL('TeaCategories', [id, name, description])
+    );
+    transaction.executeSql(
+      this.createTableSQL('Teas', [
+        id,
+        name, description,
+        { name: 'teaCategoryRid', type: 'INTEGER' }
+      ])
+    );
+  }
+
+  private createTableSQL(name: string, columns: Array<Column>): string {
+    let cols = '';
+    columns.forEach((c, i) => {
+      cols += `${i ? ', ' : ''}${c.name} ${c.type}`;
+    });
+    return `CREATE TABLE IF NOT EXISTS ${name} (${cols})`;
+  }
+}
--- src/app/services/tea-categories/tea-categories.service.ts
+++ src/app/services/tea-categories/tea-categories.service.ts
@@ -1,37 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 
-import { SQLite, SQLiteObject, DbTransaction } from '@ionic-enterprise/offline-storage/ngx';
 import { TeaCategory } from '@app/models';
-import { Platform } from '@ionic/angular';
-
-interface Column {
-  name: string;
-  type: string;
-}
+import { DatabaseService } from '@app/services/database/database.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeaCategoriesService {
-  private handle: SQLiteObject;
-  private readyPromise: Promise<void>;
-  private isReady = false;
   private changedSubject: BehaviorSubject<void>;
 
   get changed(): Observable<void> {
     return this.changedSubject.asObservable();
   }
 
-  constructor(private platform: Platform, private sqlite: SQLite) {
+  constructor(private database: DatabaseService) {
     this.changedSubject = new BehaviorSubject(null);
-    this.readyPromise = this.initializeDatabase();
   }
 
   async getAll(): Promise<Array<TeaCategory>> {
     const cats: Array<TeaCategory> = [];
-    await this.readyPromise;
-    await this.handle.transaction(tx =>
+    await this.database.ready();
+    await this.database.handle.transaction(tx =>
       tx.executeSql('SELECT * FROM TeaCategories ORDER BY name', [], (_t, r) => {
         for (let i = 0; i < r.rows.length; i++) {
           cats.push(r.rows.item(i));
@@ -43,8 +33,8 @@ export class TeaCategoriesService {
 
   async get(id: string): Promise<TeaCategory> {
     let cat: TeaCategory = null;
-    await this.readyPromise;
-    await this.handle.transaction(tx => {
+    await this.database.ready();
+    await this.database.handle.transaction(tx => {
       tx.executeSql('SELECT * FROM TeaCategories WHERE id = ? ORDER BY name', [id], (_t, r) => {
         if (r.rows.length) {
           cat = { ...r.rows.item(0) };
@@ -59,15 +49,15 @@ export class TeaCategoriesService {
   }
 
   async delete(id: string): Promise<void> {
-    await this.readyPromise;
-    await this.handle.transaction(tx => tx.executeSql('DELETE FROM TeaCategories WHERE id = ?', [id], () => {}));
+    await this.database.ready();
+    await this.database.handle.transaction(tx => tx.executeSql('DELETE FROM TeaCategories WHERE id = ?', [id], () => {}));
     this.changedSubject.next();
   }
 
   private async add(category: TeaCategory): Promise<TeaCategory> {
-    await this.readyPromise;
+    await this.database.ready();
     const cat = { ...category };
-    await this.handle.transaction(tx  => {
+    await this.database.handle.transaction(tx  => {
       tx.executeSql('SELECT COALESCE(MAX(id), 0) + 1 AS newId FROM TeaCategories', [], (_t, r) => {
         cat.id = r.rows.item(0).newId;
         tx.executeSql(
@@ -82,8 +72,8 @@ export class TeaCategoriesService {
   }
 
   private async update(category: TeaCategory): Promise<TeaCategory> {
-    await this.readyPromise;
-    this.handle.transaction(tx => {
+    await this.database.ready();
+    this.database.handle.transaction(tx => {
       tx.executeSql(
         'UPDATE TeaCategories SET name = ?, description = ? WHERE id = ?',
         [category.name, category.description, category.id],
@@ -92,43 +82,5 @@ export class TeaCategoriesService {
     });
     this.changedSubject.next();
     return category;
-  }
-
-  private async initializeDatabase(): Promise<void> {
-    await this.platform.ready();
-    await this.open();
-    await this.handle.transaction(tx => {
-      this.createTables(tx);
-    });
-    this.isReady = true;
-  }
-
-  private async open(): Promise<void> {
-    this.handle = await this.sqlite.create({
-      name: 'teaisforme.db',
-      location: 'default'
-    });
-  }
-
-  private createTables(transaction: DbTransaction): void {
-    const id = { name: 'id', type: 'INTEGER PRIMARY KEY' };
-    const name = { name: 'name', type: 'TEXT' };
-    const description = { name: 'description', type: 'TEXT' };
-    transaction.executeSql(this.createTableSQL('TeaCategories', [id, name, description]));
-  }
-
-  private createTableSQL(name: string, columns: Array<Column>): string {
-    let cols = '';
-    columns.forEach((c, i) => {
-      cols += `${i ? ', ' : ''}${c.name} ${c.type}`;
-    });
-    return `CREATE TABLE IF NOT EXISTS ${name} (${cols})`;
-  }
-
-  private async ready(): Promise<boolean> {
-    if (!this.isReady) {
-      await this.readyPromise;
-    }
-    return true;
   }
 }
